a0ecab60b300d63e52a36208d2f2518092010a88
Kanikabudhiraja <kanikabudhiraja.xb@gmail.com>
2025-01-02 21:44:08 +0530

    Upadted Javascript files

---
+++ Basics/05_basis.js
@@ -0,0 +1,26 @@
+//Comparision operator
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1)
+console.log(2==1)
+console.log(2!=1)
+
+console.log("2" >1);
+console.log("02">1);
+
+console.log(null>0)
+console.log(null>=0) //true only in this case
+console.log(null==0)
+
+console.log(undefined ==0 ) // always give false always 
+console.log(undefined >0 )
+console.log(undefined >=0 )
+
+// strict check ===
+// also check it's data type
+
+console.log("2" === 2); // we are checking for strings and number 
+
+
+
