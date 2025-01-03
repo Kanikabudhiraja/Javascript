6dbe719a54ec2ce040546597dd57f8a1d71ac3e8
Kanikabudhiraja <kanikabudhiraja.xb@gmail.com>
2025-01-03 10:33:43 +0530

    Updated Javascript Files

---
+++ Basics/datatype_summ.js
@@ -0,0 +1,45 @@
+//premitive and nonpremitive datatype
+
+// primitive(call by value)-->original reference nhi dia jata copy m changes hote h 
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol(unique value banne k lie kia jata h), BigInt
+
+// Reference type(Non primitive)
+// Array, object , function
+
+// Javascript -> statically 
+
+// const score = 100
+// const scoreValue= 100.3
+// const isLoggedIn = false
+// const outsideTemp =null
+// let userEmail; // undefined value
+
+// defining a symbol
+// const id =Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId)//false return value same nhi h 
+
+const bigNumber = 786664484894849
+
+const heroes =["shaktiman", "naagraj","doga"];
+
+let myObj ={
+    name: "kanika",
+    age: 22,
+}
+
+const myFunction = function()
+{
+    console.log("Hello World");
+}
+
+console.log(typeof myFunction)
+
+// null k typeof s datatype ->object aata h 
+// object k function aata h
+
+
+
+
+
---
+++ Basics/memory.js
@@ -0,0 +1,27 @@
+// mmeoryspace 
+
+// stack (permitive) --> variable k copy milta h 
+// Heap(non primitive) -->  variable decalre hota tha toh orginal value k refrence milta h aur jo bhi chnages hote h vo original value m bhi reflect hota h 
+
+// let myYoutubename="kanikabudhiraja"
+// let anothername= myYoutubename
+// console.log(myYoutubename)
+// console.log(anothername);
+
+let myYoutubename="kanikabudhiraja"
+let anothername= myYoutubename
+anothername= "techwithKANIKA"
+console.log(myYoutubename)
+console.log(anothername);
+
+let userOne=
+{
+    email: "user.@gmail.com",
+    upi:"user@upi"
+}
+
+let userTwo= userOne;
+
+userTwo.email="kanika@google.com";
+console.log(userOne.email)
+console.log(userTwo.email)
